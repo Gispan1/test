126ed23e75fd607685ea1019f86d7d1b7f988f48
Tal Gispan <tal.gispan@biocatch.com>
2017-07-14 11:55:53 +0300

    cart max amount changed back to 500

--- e2e.py
+++ e2e.py
@@ -4,7 +4,7 @@ def purchase_runner(item_name="dvd player"):
     website = session_module.amazon()
     website.open_amzon_site()
     website.search_items(item_name)
-    website.add_items_to_cart(max_amount=100)
+    website.add_items_to_cart(max_amount=500)
     website.proceed_to_checkout()
     website.fill_user_details('automation_user','automation_user@malinator.com','somepassword')
     website.end_session()
